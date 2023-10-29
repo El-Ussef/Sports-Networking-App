b13c3ce133a4a0817dad2607643b4d6405ec32b3
Ussef <elmanssouri.youssef96@gmail.com>
2023-10-29 23:19:28 +0100

    domain updated

--- Domain/Entities/AthleteDocument.cs
+++ Domain/Entities/AthleteDocument.cs
@@ -1,10 +1,11 @@
+using Domain.Common;
 using Domain.Enums;
 
 namespace Domain.Entities;
 
-public class Document
+
+public class Document : BaseEntity
 {
-    public int Id { get; set; }
     public int AthleteId { get; set; }
     public Athlete Athlete { get; set; }
     public string FilePath { get; set; }
--- Domain/Entities/MedicalStaff.cs
+++ Domain/Entities/MedicalStaff.cs
@@ -1,6 +1,6 @@
 namespace Domain.Entities;
 
-public class Payment
+public class MedicalStaff
 {
     
 }
